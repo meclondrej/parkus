8bd1933182b325a5bc58123fcacf79dac3205e51
Ondrej Mecl <meclondrej@protonmail.com>
2024-01-12 16:50:23 +0100

    Changed strings in ConnectionStatusBroadcast to czech

--- features/ConnectionStatusBroadcast.cs
+++ features/ConnectionStatusBroadcast.cs
@@ -9,17 +9,17 @@ namespace parkus.features
         [PluginEvent]
         public void OnPlayerPreauth(PlayerPreauthEvent ev)
         {
-            Server.SendBroadcast("A player is joining...", 10, Broadcast.BroadcastFlags.Normal, true);
+            Server.SendBroadcast("Hráč se připojuje...", 10, Broadcast.BroadcastFlags.Normal, true);
         }
         [PluginEvent]
         public void OnPlayerJoined(PlayerJoinedEvent ev)
         {
-            Server.SendBroadcast($"{ev.Player.Nickname} joined the server.", 10, Broadcast.BroadcastFlags.Normal, true);
+            Server.SendBroadcast($"{ev.Player.Nickname} se připojil do hry.", 10, Broadcast.BroadcastFlags.Normal, true);
         }
         [PluginEvent]
         public void OnPlayerLeft(PlayerLeftEvent ev)
         {
-            Server.SendBroadcast($"{ev.Player.Nickname} left the server.", 10, Broadcast.BroadcastFlags.Normal, true);
+            Server.SendBroadcast($"{ev.Player.Nickname} opustil hru.", 10, Broadcast.BroadcastFlags.Normal, true);
         }
     }
 }
