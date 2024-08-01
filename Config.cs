b2fcaca946db07c2cf0417ba279dc5a72f442c5f
Ondrej Mecl <meclondrej@protonmail.com>
2024-08-01 23:28:50 +0200

    Fixed invalid config impl

--- Config.cs
+++ Config.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel;
 using Exiled.API.Interfaces;
 
 namespace parkus
 {
     public class Config : IConfig
     {
-        bool IConfig.IsEnabled { get; set; } = true;
-        bool IConfig.Debug { get; set; } = false;
+        [Description("Whether or not this plugin is enabled")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Whether or not debug messages should be shown in the console")]
+        public bool Debug { get; set; } = false;
     }
 }
