3a6b4c17b4a610e07d42452f1ea15290ee9b44a6
meclondrej <ondrej.mecl@volny.cz>
2025-09-28 22:01:31 +0200

    Added alpha warhead and escape count info to the respawn timer

--- Features/RespawnTimer.cs
+++ Features/RespawnTimer.cs
@@ -9,6 +9,7 @@ using PlayerRoles;
 using Respawning;
 using Respawning.Waves;
 using MEC;
+using Exiled.API.Enums;
 
 namespace parkus.Features
 {
@@ -81,9 +82,48 @@ public class RespawnTimer
         return $"{span.TotalMinutes:00}:{span.Seconds:00}";
     }
 
+    private static string GetWarheadText()
+    {
+        if (Warhead.IsLocked)
+            return "zamčená";
+        StringBuilder str = new StringBuilder();
+        WarheadStatus warheadStatus = Warhead.Status;
+        int time = 0;
+        switch (warheadStatus)
+        {
+        case WarheadStatus.NotArmed:
+            str.Append("<color=#00FF00>zajištěná</color>");
+            time = (int)Warhead.RealDetonationTimer;
+            break;
+        case WarheadStatus.Armed:
+            str.Append("<color=#FF8C00>odjištěná</color>");
+            time = (int)Warhead.RealDetonationTimer;
+            break;
+        case WarheadStatus.InProgress:
+            str.Append("<color=#FF0000>aktivní</color>");
+            time = (int)Warhead.DetonationTimer;
+            break;
+        case WarheadStatus.Detonated:
+            str.Append("odpálená");
+            time = (int)Warhead.DetonationTimer;
+            break;
+        }
+        str.Append($" {time / 60:00}:{time % 60:00}");
+        return str.ToString();
+    }
+
+    private const string TimerTextTemplate = "Další spawn jako: {nextrole}\n"
+        + "<color=#3333FF>NTF</color>: {ntftime}\n"
+        + "<color=#3333FF>NTF</color> MINI: {ntfminitime}\n"
+        + "<color=#33FF33>CHAOS</color>: {chaostime}\n"
+        + "<color=#33FF33>CHAOS</color> MINI: {chaosminitime}\n"
+        + "Alpha warhead: {warheadstate}\n"
+        + "Class-D uteklo: {classdescape}\n"
+        + "Scientistů uteklo: {scientistescape}\n";
+
     private string GenerateTimerText()
     {
-        StringBuilder builder = new StringBuilder("Další spawn jako: {nextrole}\n<color=#3333FF>NTF</color>: {ntftime}\n<color=#3333FF>NTF</color> MINI: {ntfminitime}\n<color=#33FF33>CHAOS</color>: {chaostime}\n<color=#33FF33>CHAOS</color> MINI: {chaosminitime}");
+        StringBuilder builder = new StringBuilder(TimerTextTemplate);
         switch (WaveManager._nextWave?.TargetFaction)
         {
         case Faction.FoundationStaff: // NTF
@@ -100,6 +140,9 @@ public class RespawnTimer
         builder.Replace("{ntfminitime}", GetWaveRespawnText<NtfMiniWave>());
         builder.Replace("{chaostime}", GetWaveRespawnText<ChaosSpawnWave>());
         builder.Replace("{chaosminitime}", GetWaveRespawnText<ChaosMiniWave>());
+        builder.Replace("{warheadstate}", GetWarheadText());
+        builder.Replace("{classdescape}", Round.EscapedDClasses.ToString());
+        builder.Replace("{scientistescape}", Round.EscapedScientists.ToString());
         return builder.ToString();
     }
 }
